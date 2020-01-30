11c68c07cd6712c8bb3fc68aa3509d1e191ebd6a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-30 21:10:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def initialize(dogs_name)
+        @name = name
+    end
+    def name
+      return @name
+    end
+    def breed(dogs_breed)
+      @breed = dogs_breed
+    end
+    def breeds 
+      return @breed
+    end
+end
